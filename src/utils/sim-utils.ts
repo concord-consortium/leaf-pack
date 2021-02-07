825ed66386d2591a6a424bc4243ebb65829faadd
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-07 09:56:18 -0800

    Remove obsolete exports from sim-utils

--- src/utils/sim-utils.ts
+++ src/utils/sim-utils.ts
@@ -68,7 +68,6 @@ export interface Environment {
   type: EnvironmentType;
   name: string;
   backgroundImage: any;
-  enabled: boolean; // some environments are off in DESE version
 }
 
 export const Environments: Environment[] = [
@@ -76,49 +75,29 @@ export const Environments: Environment[] = [
     type: EnvironmentType.environment1,
     name: t("ENVIRONMENT.1"),
     backgroundImage: EnvironmentImage1,
-    enabled: true,
   },
   {
     type: EnvironmentType.environment2,
     name: t("ENVIRONMENT.2"),
     backgroundImage: EnvironmentImage2,
-    enabled: true,
   },
   {
     type: EnvironmentType.environment3,
     name: t("ENVIRONMENT.3"),
     backgroundImage: EnvironmentImage3,
-    enabled: true,
   },
   {
     type: EnvironmentType.environment4,
     name: t("ENVIRONMENT.4"),
     backgroundImage: EnvironmentImage4,
-    enabled: true,
   },
 ];
-
-export interface ScaleDisplayBlock {
-  color: string;
-  label: string;
-}
 
 export enum LeafDecompositionType {
   little = "little",
   some = "some",
   lots = "lots",
 }
-
-export interface LeafDecomposition {
-  type: LeafDecompositionType;
-  scaleDisplay: ScaleDisplayBlock;
-}
-
-export const LeafDecompositions: LeafDecomposition[] =
-  [{ type: LeafDecompositionType.little, scaleDisplay: { color: "#ffe293", label: t("LEAFDECOMPOSITIONAMOUNT.LEVEL0") } },
-   { type: LeafDecompositionType.some, scaleDisplay: { color: "#ebea70", label: t("LEAFDECOMPOSITIONAMOUNT.LEVEL1") } },
-   { type: LeafDecompositionType.lots, scaleDisplay: { color: "#b6dcae", label: t("LEAFDECOMPOSITIONAMOUNT.LEVEL2") } }
-  ];
 
 export interface LeafDecompositionFinalValue {
   value: LeafDecompositionType;
@@ -137,25 +116,11 @@ export const LeafDecompositionFinalValues: LeafDecompositionFinalValue[] = [
   { environment: EnvironmentType.environment4, value: LeafDecompositionType.little, sunny: true}, // TODO: get final val
 ];
 
-
 export enum LeafEatersAmountType {
   few = "few",
   some = "some",
   lots = "lots",
 }
-
-export interface LeafEatersAmount {
-  type: LeafEatersAmountType;
-  scaleDisplay: ScaleDisplayBlock;
-  max: number;
-}
-
-// for now this uses the caddisfly count: >5 is some, >8 is alot
-export const LeafEatersAmounts: LeafEatersAmount[] =
-  [{ type: LeafEatersAmountType.few, scaleDisplay: { color: "#ffe293", label: t("LEAFEATERSAMOUNT.LEVEL0") }, max: 5 },
-   { type: LeafEatersAmountType.some, scaleDisplay: { color: "#ebea70", label: t("LEAFEATERSAMOUNT.LEVEL1") }, max: 8 },
-   { type: LeafEatersAmountType.lots, scaleDisplay: { color: "#b6dcae", label: t("LEAFEATERSAMOUNT.LEVEL2") }, max: 9999 }
-  ];
 
 export interface LeafEatersFinalValue {
   value: LeafEatersAmountType;
@@ -180,19 +145,6 @@ export enum AlgaeEatersAmountType {
   lots = "lots",
 }
 
-export interface AlgaeEatersAmount {
-  type: AlgaeEatersAmountType;
-  scaleDisplay: ScaleDisplayBlock;
-  max: number;
-}
-
-// for now this uses the midgefly count: >10 is some, >16 is alot
-export const AlgaeEatersAmounts: AlgaeEatersAmount[] =
-  [{ type: AlgaeEatersAmountType.few, scaleDisplay: { color: "#ffe293", label: t("ALGAEEATERSAMOUNT.LEVEL0") }, max: 10 },
-   { type: AlgaeEatersAmountType.some, scaleDisplay: { color: "#ebea70", label: t("ALGAEEATERSAMOUNT.LEVEL1") }, max: 16 },
-   { type: AlgaeEatersAmountType.lots, scaleDisplay: { color: "#b6dcae", label: t("ALGAEEATERSAMOUNT.LEVEL2") }, max: 9999 }
-  ];
-
 export interface AlgaeEatersFinalValue {
   value: AlgaeEatersAmountType;
   environment: EnvironmentType;
@@ -216,19 +168,6 @@ export enum FishAmountType {
   lots = "lots",
 }
 
-export interface FishAmount {
-  type: FishAmountType;
-  scaleDisplay: ScaleDisplayBlock;
-  max: number;
-}
-
-// for now this uses the crayfish count: >2 is some, >4 is alot
-export const FishAmounts: FishAmount[] =
-  [{ type: FishAmountType.few, scaleDisplay: { color: "#ffe293", label: t("FISHAMOUNT.LEVEL0") }, max: 2 },
-   { type: FishAmountType.some, scaleDisplay: { color: "#ebea70", label: t("FISHAMOUNT.LEVEL1") }, max: 4 },
-   { type: FishAmountType.lots, scaleDisplay: { color: "#b6dcae", label: t("FISHAMOUNT.LEVEL2") }, max: 9999 }
-  ];
-
 export interface FishFinalValue {
   value: FishAmountType;
   environment: EnvironmentType;
@@ -245,11 +184,6 @@ export const FishFinalValues: FishFinalValue[] = [
   { environment: EnvironmentType.environment4, value: FishAmountType.few, sunny: false},
   { environment: EnvironmentType.environment4, value: FishAmountType.few, sunny: true},
 ];
-
-export enum AnimalSize {
-  small = "small",
-  large = "large",
-}
 
 export enum AnimalType {
   stoneFly = "stoneFly",
@@ -291,144 +225,143 @@ export interface Animal {
   type: AnimalType;
   label: string;
   abundance: Record<EnvironmentType, EnvironmentAbundance>;
-  size: AnimalSize;
   image: any;
 }
 
 export const Animals: Animal[] = [
-  { type: AnimalType.stoneFly, label: t("ANIMAL.STONEFLY"), size: AnimalSize.small, image: stoneFlyImage,
+  { type: AnimalType.stoneFly, label: t("ANIMAL.STONEFLY"), image: stoneFlyImage,
     abundance: { "environment1": {sunny: {min: 2, max: 6}, notSunny: { min: 2, max: 6 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.mayFly, label: t("ANIMAL.MAYFLY"), size: AnimalSize.small, image: mayFlyImage,
+  { type: AnimalType.mayFly, label: t("ANIMAL.MAYFLY"), image: mayFlyImage,
     abundance: { "environment1": {sunny: {min: 10, max: 20}, notSunny: { min: 10, max: 20 }},
                  "environment2": {sunny: {min: 10, max: 20}, notSunny: { min: 10, max: 20 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.caddisFly, label: t("ANIMAL.CADDISFLY"), size: AnimalSize.small, image: caddisFlyImage,
+  { type: AnimalType.caddisFly, label: t("ANIMAL.CADDISFLY"), image: caddisFlyImage,
     abundance: { "environment1": {sunny: {min: 8, max: 12}, notSunny: { min: 8, max: 12 }},
                  "environment2": {sunny: {min: 8, max: 12}, notSunny: { min: 8, max: 12 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.dobsonFly, label: t("ANIMAL.DOBSONFLY"), size: AnimalSize.small, image: dobsonFlyImage,
+  { type: AnimalType.dobsonFly, label: t("ANIMAL.DOBSONFLY"), image: dobsonFlyImage,
     abundance: { "environment1": {sunny: {min: 1, max: 3}, notSunny: { min: 1, max: 3 }},
                  "environment2": {sunny: {min: 1, max: 3}, notSunny: { min: 1, max: 3 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.riffleBeatle, label: t("ANIMAL.RIFFLEBEATLE"), size: AnimalSize.small, image: riffleBeatleImage,
+  { type: AnimalType.riffleBeatle, label: t("ANIMAL.RIFFLEBEATLE"), image: riffleBeatleImage,
     abundance: { "environment1": {sunny: {min: 6, max: 10}, notSunny: { min: 6, max: 10 }},
                  "environment2": {sunny: {min: 7, max: 9}, notSunny: { min: 7, max: 8 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.waterPenny, label: t("ANIMAL.WATERPENNY"), size: AnimalSize.small, image: waterPennyImage,
+  { type: AnimalType.waterPenny, label: t("ANIMAL.WATERPENNY"), image: waterPennyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.gilledSnail, label: t("ANIMAL.GILLEDSNAIL"), size: AnimalSize.small, image: gilledSnailImage,
+  { type: AnimalType.gilledSnail, label: t("ANIMAL.GILLEDSNAIL"), image: gilledSnailImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.snipeFly, label: t("ANIMAL.SNIPEFLY"), size: AnimalSize.small, image: snipeFlyImage,
+  { type: AnimalType.snipeFly, label: t("ANIMAL.SNIPEFLY"), image: snipeFlyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.damselFly, label: t("ANIMAL.DAMSELFLY"), size: AnimalSize.small, image: damselFlyImage,
+  { type: AnimalType.damselFly, label: t("ANIMAL.DAMSELFLY"), image: damselFlyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.dragonFly, label: t("ANIMAL.DRAGONFLY"), size: AnimalSize.small, image: dragonFlyImage,
+  { type: AnimalType.dragonFly, label: t("ANIMAL.DRAGONFLY"), image: dragonFlyImage,
     abundance: { "environment1": {sunny: {min: 6, max: 8}, notSunny: { min: 6, max: 8 }},
                  "environment2": {sunny: {min: 4, max: 6}, notSunny: { min: 4, max: 6 }},
                  "environment3": {sunny: {min: 1, max: 3}, notSunny: { min: 1, max: 3 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.sowBug, label: t("ANIMAL.SOWBUG"), size: AnimalSize.small, image: sowBugImage,
+  { type: AnimalType.sowBug, label: t("ANIMAL.SOWBUG"), image: sowBugImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.scud, label: t("ANIMAL.SCUD"), size: AnimalSize.small, image: scudImage,
+  { type: AnimalType.scud, label: t("ANIMAL.SCUD"), image: scudImage,
     abundance: { "environment1": {sunny: {min: 7, max: 13}, notSunny: { min: 7, max: 13 }},
                  "environment2": {sunny: {min: 7, max: 13}, notSunny: { min: 7, max: 13 }},
                  "environment3": {sunny: {min: 3, max: 7}, notSunny: { min: 3, max: 7 }},
                  "environment4": {sunny: {min: 3, max: 7}, notSunny: { min: 3, max: 7 }} }
   },
-  { type: AnimalType.craneFly, label: t("ANIMAL.CRANEFLY"), size: AnimalSize.small, image: craneFlyImage,
+  { type: AnimalType.craneFly, label: t("ANIMAL.CRANEFLY"), image: craneFlyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.clamOrMussel, label: t("ANIMAL.CLAMORMUSSEL"), size: AnimalSize.small, image: clamOrMusselImage,
+  { type: AnimalType.clamOrMussel, label: t("ANIMAL.CLAMORMUSSEL"), image: clamOrMusselImage,
     abundance: { "environment1": {sunny: {min: 2, max: 4}, notSunny: { min: 2, max: 4 }},
                  "environment2": {sunny: {min: 2, max: 4}, notSunny: { min: 2, max: 4 }},
                  "environment3": {sunny: {min: 2, max: 4}, notSunny: { min: 2, max: 4 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.crayFish, label: t("ANIMAL.CRAYFISH"), size: AnimalSize.large, image: crayFishImage,
+  { type: AnimalType.crayFish, label: t("ANIMAL.CRAYFISH"), image: crayFishImage,
     abundance: { "environment1": {sunny: {min: 3, max: 5}, notSunny: { min: 3, max: 5 }},
                  "environment2": {sunny: {min: 1, max: 3}, notSunny: { min: 1, max: 3 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.netSpinningCaddisFly, label: t("ANIMAL.NETSPINNINGCADDISFLY"), size: AnimalSize.small, image: netSpinningCaddisFlyImage,
+  { type: AnimalType.netSpinningCaddisFly, label: t("ANIMAL.NETSPINNINGCADDISFLY"), image: netSpinningCaddisFlyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.midgeFly, label: t("ANIMAL.MIDGEFLY"), size: AnimalSize.small, image: midgeFlyImage,
+  { type: AnimalType.midgeFly, label: t("ANIMAL.MIDGEFLY"), image: midgeFlyImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 16, max: 24}, notSunny: { min: 16, max: 24 }},
                  "environment4": {sunny: {min: 40, max: 60}, notSunny: { min: 40, max: 60 }} }
   },
-  { type: AnimalType.blackFly, label: t("ANIMAL.BLACKFLY"), size: AnimalSize.small, image: blackFlyImage,
+  { type: AnimalType.blackFly, label: t("ANIMAL.BLACKFLY"), image: blackFlyImage,
     abundance: { "environment1": {sunny: {min: 21, max: 29}, notSunny: { min: 21, max: 29 }},
                  "environment2": {sunny: {min: 12, max: 18}, notSunny: { min: 12, max: 18 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.planarian, label: t("ANIMAL.PLANARIAN"), size: AnimalSize.small, image: planarianImage,
+  { type: AnimalType.planarian, label: t("ANIMAL.PLANARIAN"), image: planarianImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 4, max: 6}, notSunny: { min: 4, max: 6 }},
                  "environment4": {sunny: {min: 4, max: 6}, notSunny: { min: 4, max: 6 }} }
   },
-  { type: AnimalType.leech, label: t("ANIMAL.LEECH"), size: AnimalSize.small, image: leechImage,
+  { type: AnimalType.leech, label: t("ANIMAL.LEECH"), image: leechImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 1, max: 3}, notSunny: { min: 1, max: 3 }},
                  "environment4": {sunny: {min: 2, max: 4}, notSunny: { min: 2, max: 4 }} }
   },
-  { type: AnimalType.lungedSnail, label: t("ANIMAL.LUNGEDSNAIL"), size: AnimalSize.small, image: lungedSnailImage,
+  { type: AnimalType.lungedSnail, label: t("ANIMAL.LUNGEDSNAIL"), image: lungedSnailImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.aquaticWorm, label: t("ANIMAL.AQUATICWORM"), size: AnimalSize.small, image: aquaticWormImage,
+  { type: AnimalType.aquaticWorm, label: t("ANIMAL.AQUATICWORM"), image: aquaticWormImage,
     abundance: { "environment1": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment2": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment3": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }},
                  "environment4": {sunny: {min: 0, max: 0}, notSunny: { min: 0, max: 0 }} }
   },
-  { type: AnimalType.ratTailedMaggot, label: t("ANIMAL.RATTAILEDMAGGOT"), size: AnimalSize.small, image: ratTailedMaggotImage,
+  { type: AnimalType.ratTailedMaggot, label: t("ANIMAL.RATTAILEDMAGGOT"), image: ratTailedMaggotImage,
     abundance: { "environment1": {sunny: {min: 4, max: 6}, notSunny: { min: 4, max: 6 }},
                  "environment2": {sunny: {min: 4, max: 6}, notSunny: { min: 4, max: 6 }},
                  "environment3": {sunny: {min: 5, max: 9}, notSunny: { min: 5, max: 9 }},
