1d9372a559f1fb4ac3056223cc1364682270141e
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-07 22:13:57 -0800

    Add habitat panel and update util exports

--- src/components/notebook/habitat-panel.tsx
+++ src/components/notebook/habitat-panel.tsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { SectionButtons } from "./section-buttons";
+import { EnvironmentType, habitatCategories, Environments } from "../../utils/sim-utils";
+import CheckIcon from "../../assets/check-icon.svg";
 
 import "./habitat-panel.scss";
 
+const kCategoriesPerSection = 3;
+
 interface IProps {
+  environment: EnvironmentType;
+  featureSelections: boolean[];
+  onSelectFeature: (index: number, selected: boolean) => void;
   isRunning: boolean;
 }
 
 export const HabitatPanel: React.FC<IProps> = (props) => {
+  const { environment, featureSelections, onSelectFeature } = props;
+  const [currentSection, setCurrentSection] = useState(0);
+  const numSections = Math.ceil(habitatCategories.length / kCategoriesPerSection);
+  const environmentImage = Environments.find((env) => env.type === environment)?.sketchImage;
+
+  const featureOrder: string[] = [];
+  habitatCategories.forEach((hCat) => {
+    hCat.features?.forEach((feat) => {
+      featureOrder.push(feat);
+    });
+  });
+
   return (
     <div className="habitat-panel">
-      Habitat Panel
+      <div className="categories">
+        {habitatCategories.map((category, cIndex) =>
+          cIndex >= kCategoriesPerSection * currentSection &&
+          cIndex < kCategoriesPerSection * currentSection + kCategoriesPerSection &&
+          <div key={`habitat-category-${cIndex}`} className="category">
+            <div className="header">{category.title}</div>
+            { category.features
+              ? category.features.map((feature, fIndex) =>
+                  <div className="feature-row" key={`feature-row-${fIndex}`}>
+                    <button
+                      className={`checkbox ${featureSelections[featureOrder.findIndex((f) => f === feature)] ? "selected" : ""}`}
+                      onClick={() => onSelectFeature(featureOrder.findIndex((f) => f === feature),
+                        !featureSelections[featureOrder.findIndex((f) => f === feature)])}
+                    >
+                      <CheckIcon className="check" />
+                    </button>
+                    <div key={`feature-${fIndex}`}>{feature}</div>
+                  </div>
+                )
+              : <img src={environmentImage} />
+            }
+          </div>
+        )}
+      </div>
+      <SectionButtons
+        currentSection={currentSection}
+        totalSections={numSections}
+        onSelectSection={setCurrentSection}
+      />
     </div>
   );
 };
