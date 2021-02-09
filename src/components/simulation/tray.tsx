6f39b8ce4be52e65a0340d3e780a403699150c15
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-08 20:45:13 -0800

    Convert duplicate inline SVG to file

--- src/components/simulation/tray.tsx
+++ src/components/simulation/tray.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import t from "../../utils/translation/translate";
 import SortingTray from "../../assets/sorting-tray.svg";
+import CloseIcon from "../../assets/close-icon.svg";
 import { Animal, Animals, TrayAnimal } from "../../utils/sim-utils";
 
 import "./tray.scss";
@@ -20,10 +21,7 @@ export const Tray: React.FC<IProps> = (props) => {
       <div className="header">
         <div className="title">{t("SORTINGTRAY")}</div>
         <button className="close" onClick={onHideTray}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" width="12" height="12">
-            <line x1="0" y1="0" x2="12" y2="12" strokeWidth="2.5"/>
-            <line x1="12" y1="0" x2="0" y2="12" strokeWidth="2.5"/>
-          </svg>
+          <CloseIcon />
         </button>
       </div>
       { trayAnimals.map((ta, index) => {
