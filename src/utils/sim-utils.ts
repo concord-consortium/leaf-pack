abb10bf9ec55eca4afb9fd368c63760f1d8902dd
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-10 12:38:05 -0800

    Update PTI colors to match spec

--- src/utils/sim-utils.ts
+++ src/utils/sim-utils.ts
@@ -484,8 +484,8 @@ export interface PTIRatingLevel {
 }
 
 export const PTIRatingLevels: PTIRatingLevel[] = [
-  {color: "#92e3aa", label: t("PTI.RATING.LEVEL0.LABEL"), range: t("PTI.RATING.LEVEL0.RANGE"), min: 24},
-  {color: "#7ad9f8", label: t("PTI.RATING.LEVEL1.LABEL"), range: t("PTI.RATING.LEVEL1.RANGE"), min: 17},
+  {color: "#a4f9be", label: t("PTI.RATING.LEVEL0.LABEL"), range: t("PTI.RATING.LEVEL0.RANGE"), min: 24},
+  {color: "#94e5ff", label: t("PTI.RATING.LEVEL1.LABEL"), range: t("PTI.RATING.LEVEL1.RANGE"), min: 17},
   {color: "#cccccc", label: t("PTI.RATING.LEVEL2.LABEL"), range: t("PTI.RATING.LEVEL2.RANGE"), min: 11},
   {color: "#ffacac", label: t("PTI.RATING.LEVEL3.LABEL"), range: t("PTI.RATING.LEVEL3.RANGE"), min: 0}
 ];
