bec06ae18118b48bf92a1ec78a8749edde31923f
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-15 11:09:59 -0800

    Simplify preview image calculations, unify CSS class names

--- src/components/simulation/tray-image.tsx
+++ src/components/simulation/tray-image.tsx
@@ -24,12 +24,11 @@ export const TrayImage: React.FC<IProps> = (props) => {
   // TODO: allow leaf drag
   const allowDrag = trayObject.type !== LeafType.birch && trayObject.type !== LeafType.maple && trayObject.type !== LeafType.oak;
 
-  const [{isDragging, dragPosition, dragSourcePosition}, drag ] = useDrag({
+  const [{isDragging, dragSourcePosition}, drag ] = useDrag({
     item: { type: trayObject.type, trayIndex: trayObject.trayIndex, dragImage: trayObject.dragImage,
             left: trayObject.x, top: trayObject.y },
     collect: monitor => ({
       isDragging: !!monitor.isDragging(),
-      dragPosition: monitor.getClientOffset(),
       dragSourcePosition: monitor.getSourceClientOffset()
     }),
     canDrag: allowDrag,
@@ -37,16 +36,14 @@ export const TrayImage: React.FC<IProps> = (props) => {
 
   const PreviewImage = () => {
     const {display, item} = usePreview();
-    if (!display || !dragPosition || !dragSourcePosition) {
+    if (!display || !dragSourcePosition) {
       return null;
     }
-    const dragDeltaX = dragPosition.x - dragSourcePosition.x;
-    const dragDeltaY = dragPosition.y - dragSourcePosition.y;
     const boundingBoxDeltaX = (trayObject.boundingBoxWidth - trayObject.width) / 2;
     const boundingBoxDeltaY = (trayObject.boundingBoxHeight - trayObject.height) / 2;
     const previewStyle = {
-      left: dragPosition.x - dragDeltaX + boundingBoxDeltaX - kOutlineOffset,
-      top: dragPosition.y - dragDeltaY + boundingBoxDeltaY - kOutlineOffset,
+      left: dragSourcePosition.x + boundingBoxDeltaX - kOutlineOffset,
+      top: dragSourcePosition.y + boundingBoxDeltaY - kOutlineOffset,
       transform: `rotate(${trayObject.rotation}deg)`
     };
     return <img style={previewStyle} src={item.dragImage} className="preview" />;
@@ -61,18 +58,18 @@ export const TrayImage: React.FC<IProps> = (props) => {
       {isDragging && <PreviewImage />}
       <div style={containerStyle} className="tray-image-container">
         <TrayObjectImage
-          className={`tray-image-svg ${isDragging || trayObject.type === traySelectionType ? "highlight" : ""}`}
+          className={`tray-object-image ${isDragging || trayObject.type === traySelectionType ? "highlight" : ""}`}
           style={imageStyle}
         />
         { allowDrag &&
-          <svg version="1.1" className="tray-image-clickable" style={imageStyle}>
+          <svg version="1.1" className="tray-object-image-selectable" style={imageStyle}>
             <path
               ref={drag}
               cursor="pointer"
               pointerEvents="visible"
               fillRule="evenodd"
               onClick={() => onTrayObjectSelect(trayObject.type)}
-              className="click-area"
+              className="selectable-area"
               d={trayObject.selectionPath}
             />
           </svg>
