f690b31371db84e5ef264dae043cc563e1eff998
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-16 14:09:43 -0800

    Add array shuffle util function

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -34,3 +34,19 @@ export const calculateBoundedPosition = (left: number, top: number, width: numbe
 export const getRandomInteger = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
+
+// c.f. https://github.com/Daplie/knuth-shuffle
+export const shuffleArray = (array: any[]) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
