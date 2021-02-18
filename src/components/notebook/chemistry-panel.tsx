2a242a7b5311c8c6a1c849f14f214b86efb6dc69
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-02-17 18:02:12 -0800

    Update chem utils and chem panel with test skeleton

--- src/components/notebook/chemistry-panel.tsx
+++ src/components/notebook/chemistry-panel.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+import { chemistryTests, ChemTestType, ChemistryTestResult } from "../../utils/chem-utils";
+import { getRandomInteger } from "../../utils/math-utils";
 import { SectionButtons } from "./section-buttons";
+import { ChemResults } from "./chem-results";
+import { ChemTest } from "./chem-test";
 
 import "./chemistry-panel.scss";
 
@@ -9,12 +13,50 @@ interface IProps {
 
 export const ChemistryPanel: React.FC<IProps> = (props) => {
   const [currentSection, setCurrentSection] = useState(0);
-  const numSections = 7; // TODO: derive this from export in util module
+  const [chemistryTestResults, setChemistryTestResults] = useState([
+    {type: ChemTestType.airTemperature, stepsComplete: 0, value: 0},
+    {type: ChemTestType.waterTemperature, stepsComplete: 0, value: 0},
+    {type: ChemTestType.pH, stepsComplete: 0, value: 0},
+    {type: ChemTestType.nitrate, stepsComplete: 0, value: 0},
+    {type: ChemTestType.turbidity, stepsComplete: 0, value: 0},
+    {type: ChemTestType.dissolvedOxygen, stepsComplete: 0, value: 0}
+  ]);
+
+  // TODO: move this to the model state
+  const handleUpdateTestResult = (type: ChemTestType, completedStep: number) => {
+    const updatedChemistryTestResults = chemistryTestResults.map((result: ChemistryTestResult) => {
+      if (result.type === type) {
+        const updatedResult = { ...result };
+        updatedResult.stepsComplete = completedStep;
+        const currentTest = chemistryTests.find((test) => test.type === type);
+        if (completedStep === currentTest?.steps.length) {
+          updatedResult.value = currentTest.values[getRandomInteger(0, currentTest.values.length - 1)].value;
+        }
+        return updatedResult;
+      } else {
+        return result;
+      }
+    });
+    setChemistryTestResults(updatedChemistryTestResults);
+  };
+
+  // one extra page for the result summary
+  const numSections = chemistryTests.length + 1;
 
   return (
     <div className="chemistry-panel">
       <div className="section-content">
-        Chemistry Panel
+        {currentSection === 0
+          ? <ChemResults
+              chemistryTestResults={chemistryTestResults}
+            />
+          : <ChemTest
+              chemistryTest={chemistryTests[currentSection - 1]}
+              testIndex={currentSection - 1}
+              chemistryTestResults={chemistryTestResults}
+              onUpdateTestResult={handleUpdateTestResult}
+            />
+        }
       </div>
       <SectionButtons
         currentSection={currentSection}
