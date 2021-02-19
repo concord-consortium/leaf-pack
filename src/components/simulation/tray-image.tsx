57210a4bb331d2ce4ff397d4320c485ce80a33a0
Kirk Swenson <kswenson@concord.org>
2021-02-18 22:52:03 -0800

    Fix react warning on drag

--- src/components/simulation/tray-image.tsx
+++ src/components/simulation/tray-image.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import { TrayType, TrayObject, draggableLeafTypes } from "../../utils/sim-utils";
 import { useDrag } from "react-dnd";
-import { usePreview } from "react-dnd-preview";
+import { TrayImagePreview } from "./tray-image-preview";
+import { TrayType, TrayObject } from "../../utils/sim-utils";
 
 import "./tray-image.scss";
-
-// the drag ref is attached to the clickable outline which is a little smaller than the actual
-// SVG dimensions which include the highlight border. To get the drag preview to display properly
-// we need to take into account how much the clickable outline is offset from the SVG upper-left
-// corner. This ends up being about 5 pixels for all tray objects. Technically it could be computed
-// by hand for ALL tray objects, but subtracting 5 pixels works pretty well in practice.
-const kOutlineOffset = 5;
-const kNonTrayPreviewHeight = 48;
 
 interface IProps {
   trayObject: TrayObject;
@@ -24,7 +16,7 @@ export const TrayImage: React.FC<IProps> = (props) => {
   const { trayObject, onTrayObjectSelect, traySelectionType, trayWrapper } = props;
   const TrayObjectImage = trayObject.image;
 
-  const [{isDragging, dragSourcePosition, dragPosition}, drag ] = useDrag({
+  const [{isDragging, ...otherDragProps}, drag ] = useDrag({
     item: { type: trayObject.type, trayIndex: trayObject.trayIndex, dragImage: trayObject.dragImage,
             left: trayObject.left, top: trayObject.top },
     collect: monitor => ({
@@ -34,44 +26,14 @@ export const TrayImage: React.FC<IProps> = (props) => {
     }),
   });
 
-  const PreviewImage = () => {
-    const {display, item} = usePreview();
-    if (!display || !dragSourcePosition || !dragPosition) {
-      return null;
-    }
-    const boundingBoxDeltaX = (trayObject.boundingBoxWidth - trayObject.width) / 2;
-    const boundingBoxDeltaY = (trayObject.boundingBoxHeight - trayObject.height) / 2;
-    let previewStyle: React.CSSProperties;
-    const isLeaf = draggableLeafTypes.includes(trayObject.type as any);
-
-    const trayRect = trayWrapper?.getBoundingClientRect();
-    const overTray = trayRect && (dragPosition.x >= trayRect.x && dragPosition.x <= (trayRect.x + trayRect.width) &&
-                     dragPosition.y >= trayRect.y && dragPosition.y <= (trayRect.x + trayRect.height));
-
-    if (overTray || isLeaf) {
-      previewStyle = {
-        left: dragSourcePosition.x + boundingBoxDeltaX - kOutlineOffset,
-        top: dragSourcePosition.y + boundingBoxDeltaY - kOutlineOffset,
-        transform: `rotate(${trayObject.rotation}deg)`
-      };
-    } else {
-      const previewHeight = Math.min(kNonTrayPreviewHeight, trayObject.height);
-      previewStyle = {
-        height: previewHeight,
-        left: dragPosition.x - (previewHeight / trayObject.height) * trayObject.width / 2,
-        top: dragPosition.y - previewHeight / 2,
-      };
-    }
-    return <img style={previewStyle} src={item.dragImage} className="preview" />;
-  };
-
   const containerStyle = {left: trayObject.left, top: trayObject.top, width: trayObject.width, height: trayObject.height,
                           zIndex: trayObject.zIndex};
   const imageStyle = {width: trayObject.width, height: trayObject.height, transform: `rotate(${trayObject.rotation}deg)`};
 
   return (
     <>
-      {isDragging && <PreviewImage />}
+      {isDragging &&
+        <TrayImagePreview trayWrapper={trayWrapper} trayObject={trayObject} {...otherDragProps} />}
       <div style={containerStyle} className="tray-image-container">
         <TrayObjectImage
           className={`tray-object-image ${trayObject.type === traySelectionType ? "highlight" : ""} ${isDragging ? "dragging" : ""}`}
