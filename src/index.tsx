cde738881851e82780f013499ead1dff30e5f46f
Sam Fentress <sfentress@concord.org>
2021-02-22 22:20:02 -0500

    Use lara-interactive-api to save and load state

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,23 @@ import { App} from "./components/app";
 import { renderApp } from "./components/render-app";
 import { IModelCurrentState } from "./hooks/use-model-state";
 import { IModelInputState, IModelOutputState, IModelConfig } from "./leaf-model-types";
+import { deserialize, SerializableModelState, serialize } from "./utils/serialize-utils";
+import { getInitInteractiveMessage, setInteractiveState } from "@concord-consortium/lara-interactive-api";
 
 import "./index.scss";
 
 const app = renderApp<IModelInputState, IModelOutputState, IModelConfig>({App, logEvent, modelConfig: {}, onStateChange: handleStateChange});
 
-function handleStateChange(newState: IModelCurrentState<IModelInputState, IModelOutputState>) {
-  // do nothing yet
-}
-
 function logEvent(args: any) {
   // do nothing
 }
+
+function handleStateChange(newState: IModelCurrentState<IModelInputState, IModelOutputState>) {
+  setInteractiveState(serialize(newState));
+}
+
+getInitInteractiveMessage<SerializableModelState>().then(initMsg => {
+  if (initMsg?.mode === "runtime" && initMsg.interactiveState) {
+    app.setState(deserialize(initMsg.interactiveState));
+  }
+});
